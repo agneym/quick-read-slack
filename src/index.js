e86443e2e0eabed80de79621c006a7e811e17fc7
Agney <agney.menon@bigbinary.com>
2020-05-28 02:57:04 +0530

    add shortcut for opening modal

--- src/index.js
+++ src/index.js
@@ -8,6 +8,53 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+app.shortcut("quick_read", async ({ shortcut, ack, context, client }) => {
+  try {
+    await ack();
+
+    const result = await client.views.open({
+      token: context.botToken,
+      trigger_id: shortcut.trigger_id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "My App",
+        },
+        close: {
+          type: "plain_text",
+          text: "Close",
+        },
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>.",
+            },
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "mrkdwn",
+                text:
+                  "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>",
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+    a;
+  }
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
