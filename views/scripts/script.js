8179e0dc8f85e45d6da44d5c855551ddaf3ed8ac
Agney <agney.menon@bigbinary.com>
2020-05-30 23:01:33 +0530

    attach script

---
+++ views/scripts/script.js
@@ -0,0 +1,13 @@
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+const container = document.body;
+
+/**
+ * Create dot elements to animate and append to container
+ * @param {number} count Number of dots
+ */
+function create(count) {}
+
+function init() {
+  create();
+}
